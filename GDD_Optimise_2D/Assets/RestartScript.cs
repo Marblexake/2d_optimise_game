05d7dcd9cad541664d7aee909208ea904ae58578
Marblexake <qazx29@gmail.com>
2019-06-10 01:28:42 +0800

    Added restart button with script I SHOULD BE DONE NOW :))

---
+++ GDD_Optimise_2D/Assets/RestartScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScript : MonoBehaviour
+{
+  
+    public void Restart()
+    {
+        // Loads the scene with the index of 1 in the hierarchy 
+        SceneManager.LoadScene(1);
+    }
+}
